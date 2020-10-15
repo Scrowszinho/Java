d9e38a7f0947d70bdf2ee60b5012d46ee9d659fe
Scrowszinho <gustascrows@gmail.com>
2020-10-15 20:12:05 -0300

    Lista 02 - Variaveis - Segundo Semestre FATEC

---
+++ Lista 02 - Variaveis/ExerLoja.java
@@ -0,0 +1,8 @@
+public class ExerLoja {
+    public static void main(String[] arg){
+        int quantidade = 10, npedido = 1911356;
+        long  cproduto = 1116245455445l;
+        double valorf = 150.00;
+        System.out.printf("Número do pedido: %d\nCodigo do produto: %d\nQuantidade: %d\nValor final da compra: R$ %.2f",npedido,cproduto,quantidade,valorf);       
+    }
+}
