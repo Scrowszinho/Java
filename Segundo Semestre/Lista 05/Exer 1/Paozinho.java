9ae370b6ab6d7e32ff7d81b4971f114983b27f27
Scrowszinho <gustascrows@gmail.com>
2020-11-18 09:41:33 -0300

    Parte dos frios já feita

---
+++ Segundo Semestre/Lista 05/Exer 1/Paozinho.java
@@ -0,0 +1,36 @@
+public class Paozinho extends Produto{
+    // public Paozinho(){
+    //     super();    
+    //     super();
+    // }
+
+    // public Paozinho(double preco){
+    //     super(preco);
+    // }
+
+
+}
+// public class Banana extends Fruta {
+	
+// 	private int quantidade;
+	
+// 	public Banana () {
+// 		super();
+// 	}
+	
+// 	public Banana ( String nome ) {
+// 		super( nome );
+// 	}
+	
+// 	public Banana ( String nome, int quantidade ) {
+// 		super( nome );
+// 		setQuantidade( quantidade );
+// 	}
+	
+// 	public void setQuantidade ( int quantidade ) {
+// 		this.quantidade = quantidade;
+// 	}
+	
+// 	public int getQuantidade () {
+// 		return quantidade;
+// 	}
