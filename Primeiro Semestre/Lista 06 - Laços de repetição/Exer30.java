bf936aa566c8ae7b5be17a74703069e3d67cfb9a
Scrowszinho <gustascrows@gmail.com>
2020-12-02 19:59:35 -0300

    Trocando nome

--- Primeiro Semestre/Lista 06 - Laços de repetição/Exer30.java
+++
@@ -1,3 +0,0 @@
-public class Exer30 {
-    
-}
--- Primeiro Semestre/Lista 06 - Laços de repetição/Exer6.java
+++
@@ -1,9 +0,0 @@
-public class Exer6 {
-    public static void main(String[] args){
-        int a = 1;
-        do{
-            System.out.printf("8 X %d = %d\n",a,a*8);
-            a++;
-        }while(a <= 10);
-}
-}
