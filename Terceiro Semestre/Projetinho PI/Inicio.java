c60eb593f33bf7a66d06bbc574c31a763c32dbe6
Scrowszinho <gustascrows@gmail.com>
2020-12-11 16:58:34 -0300

    Tela Inicial do projeto arrumada

--- Terceiro Semestre/Projetinho PI/Inicio.java
+++ Terceiro Semestre/Projetinho PI/Inicio.java
@@ -1,8 +1,10 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
+
 
 public class Inicio extends JFrame {
-	JButton botao1, botao2, botao3, botao4, botao;
+	JButton botao1, botao3, botao4, botao;
 	JLabel imagem;
 	public Inicio() {
 		super("Menu Inicial");
@@ -11,27 +13,69 @@ public class Inicio extends JFrame {
 		
 		ImageIcon icone = new ImageIcon("img/icone.png");
 		setIconImage(icone.getImage());		
-		imagem = new JLabel(icone);
 
 		botao = new JButton("Comprar");
-		botao1 = new JButton("Vender");
-		botao2 = new JButton("Clientes");
+		botao1 = new JButton("Vendas");
 		botao3 = new JButton("Fornecedores");
 		botao4 = new JButton("Produtos");
+		icone.setImage(icone.getImage().getScaledInstance(200, 190, 100));
+		imagem = new JLabel(icone);
 
 		botao.setBounds(50, 20, 130, 30);
 		botao1.setBounds(50, 60, 130, 30);
-		botao2.setBounds(50, 100, 130, 30);
-		botao3.setBounds(50, 140, 130, 30);
-		botao4.setBounds(50, 180, 130, 30);
+		botao3.setBounds(50, 100, 130, 30);
+		botao4.setBounds(50, 140, 130, 30);
+		imagem.setBounds(220, 20, 200, 190);
+		
 
 		tela.add(botao1);
-		tela.add(botao2);
 		tela.add(botao3);
 		tela.add(botao4);
 		tela.add(botao);
 		tela.add(imagem);
-		
+		//
+
+		botao3.addActionListener(
+			new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+				Fornecedores fornecedores = new Fornecedores();
+				fornecedores.setVisible(true);
+				setVisible(false);
+				}
+			}
+		);
+
+		botao4.addActionListener(
+			new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+				Produtos produtos = new Produtos();
+				produtos.setVisible(true);
+				setVisible(false);
+				}
+			}
+		);
+
+		botao1.addActionListener(
+			new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+				Vendas vendas = new Vendas();
+				vendas.setVisible(true);
+				setVisible(false);
+				}
+			}
+		);
+
+		botao.addActionListener(
+			new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+				Comprar comp = new Comprar();
+				comp.setVisible(true);
+				setVisible(false);
+				}
+			}
+		);
+
+		//
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setResizable(false);
 		setSize(500, 300);
@@ -45,5 +89,11 @@ public class Inicio extends JFrame {
 		app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 	}
+	private void botao(java.awt.event.ActionEvent event){
+	Comprar obj = new Comprar();
+	obj.setVisible(true);
+	
+	}
+
 
 }
